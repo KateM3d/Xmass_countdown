ff052a3dd415568719e9249de706d61cc9997499
KateM3d <ekaterina8medvedeva@gmail.com>
2021-07-13 21:56:47 -0400

    added song

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-const myDate = new Date();
-const date = myDate.getDate();
-console.log(date);
+function christmasCountdown() {
+    const christmasDate = new Date("December 25, 2021 00:00");
+    const now = new Date();
+    const diff = christmasDate - now;
+    console.log(diff);
+
+    const msInSecond = 1000;
+    const msInMinute = 60 * 1000;
+    const msInHour = 60 * 60 * 1000;
+    const msInDay = 24 * 60 * 60 * 1000;
+
+    const displayDay = Math.floor(diff / msInDay);
+    document.querySelector(".days").textContent = displayDay;
+
+    const displayHour = Math.floor((diff % msInDay) / msInHour);
+    document.querySelector(".hours").textContent = displayHour;
+
+    const displayMinute = Math.floor((diff % msInHour) / msInMinute);
+    document.querySelector(".minutes").textContent = displayMinute;
+
+    const displaySecond = Math.floor((diff % msInMinute) / msInSecond);
+    document.querySelector(".seconds").textContent = displaySecond;
+
+    if (diff <= 0) {
+        document.querySelector(".days").textContent = 0;
+        document.querySelector(".hours").textContent = 0;
+        document.querySelector(".minutes").textContent = 0;
+        document.querySelector(".seconds").textContent = 0;
+        clearInterval(timerId);
+        console.log(timerId);
+        merryChristmas()
+    }
+
+}
+
+let timerId = setInterval(christmasCountdown, 1000);
+
+
+
+function merryChristmas() {
+    const heading = document.querySelector("h1");
+    heading.textContent = "MERRY CHRISTMAS!!! HO-HO-HO!";
+    heading.classList.add("red");
+}
+
+
+const button = document.querySelector("#myButton");
+button.addEventListener("click", function() {
+    document.querySelector("#myAudio").play();
+})
